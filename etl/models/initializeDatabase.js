19f471b8c2a2039b963adbfdb65dc1efc9245e08
Michael Chan <mhcchan@ucdavis.edu>
2021-09-21 12:56:11 -0700

    Applications Table

--- etl/models/initializeDatabase.js
+++ etl/models/initializeDatabase.js
@@ -1,3 +1,5 @@
+const { pool } = require('../helpers/database');
+
 module.exports = async function initializeDatabase() {
   console.log('Initializing Database...');
   // Drop Tables
@@ -11,7 +13,10 @@ module.exports = async function initializeDatabase() {
   // Create Applications Database
   const createApplicationsTableQuery = `
     CREATE TABLE IF NOT EXISTS categories (
-
+      id VARCHAR(20) PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      shortname VARCHAR(50) NOT NULL,
+      locale VARCHAR(10)
     );
   `;
 
@@ -20,19 +25,32 @@ module.exports = async function initializeDatabase() {
     CREATE TABLE IF NOT EXISTS users (
       id INTEGER PRIMARY KEY,
       name VARCHAR(50) NOT NULL,
-      summonerLevel INTEGER NOT NULL,
-      profileIcon INTEGER,
+      summonerlevel INTEGER NOT NULL,
+      profileicon INTEGER,
       realm VARCHAR(5) NOT NULL,
-      isModerator BOOLEAN DEFAULT false,
-      isRioter BOOLEAN DEFAULT false,
-      createdAt TIMESTAMP WITH TIME ZONE,
-      banEndsAt TIMESTAMP WITH TIME ZONE
+      ismoderator BOOLEAN DEFAULT false,
+      isrioter BOOLEAN DEFAULT false,
+      createdat TIMESTAMP WITH TIME ZONE,
+      banendsat TIMESTAMP WITH TIME ZONE
     );
   `;
 
   // Create Threads Database
-  const createThreadsTableQuery = ``;
+  const createThreadsTableQuery = `
+  `;
 
   // Create Comments Database
-  const createCommentsTableQuery = ``;
+  const createCommentsTableQuery = `
+  `;
+
+  const client = await pool.connect();
+  try {
+    await client.query(dropQuery);
+    await client.query(createApplicationsTableQuery);
+    await client.query(createUsersTableQuery);
+  } catch (e) {
+    console.log(e);
+  } finally {
+    client.release();
+  }
 };
