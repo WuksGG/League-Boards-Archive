6a7e30599ce7a5102202be6cd6d1f8da78e06f97
Michael Chan <mhcchan@ucdavis.edu>
2021-12-18 16:57:25 -0800

    Added (thread/comment, createdat) index for faster discussion/comment access

--- etl/models/initializeDatabase.js
+++ etl/models/initializeDatabase.js
@@ -89,6 +89,10 @@ module.exports = async function initializeDatabase() {
     await client.query(createUsersTableQuery);
     await client.query(createThreadsTableQuery);
     await client.query(createCommentsTableQuery);
+    await client.query(`CREATE INDEX ON threads (createdat)`);
+    await client.query(`CREATE INDEX ON comments (createdat)`);
+    await client.query(`CREATE INDEX thread_ts_idx ON threads (applicationid, createdat)`);
+    await client.query(`CREATE INDEX comment_ts_idx ON comments (threadid, createdat)`);
   } catch (e) {
     console.log(e);
   } finally {
