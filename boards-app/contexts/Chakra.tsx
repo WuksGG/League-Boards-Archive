91fa6a8b9c7e3053b00763308cd360950c29ed23
Michael Chan <mhcchan@ucdavis.edu>
2022-01-23 00:56:42 -0800

    set initial style

--- boards-app/contexts/Chakra.tsx
+++ boards-app/contexts/Chakra.tsx
@@ -4,8 +4,52 @@ import {
   cookieStorageManager,
   localStorageManager,
   extendTheme,
+  ThemeConfig,
+  DarkMode,
 } from '@chakra-ui/react';
 import { mode } from '@chakra-ui/theme-tools';
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+  styles: {
+    global: (props: {
+      colorMode: 'dark',
+      theme: object,
+    }) => ({
+      'body': {
+        bg: mode('light.900', 'dark.900')(props),
+        color: '#fff',
+      },
+    }),
+  },
+  colors: {
+    dark: {
+      100: "#fff", // white
+      200: "#C9C9C9", // color of time meta and system notif
+      300: "#A5A5A5", // color of duller modal
+      400: "#595959",
+      500: "#37444D", // Search Bar lighter
+      600: "#37444d",
+      700: "#2F2F2F",
+      800: "#1F2125", // Header Color, Dark Gray
+      900: "#000002",
+    }, // invert the colors
+    light: {
+      100: "#0766c2", // white
+      200: "#1F2125", // color of time meta and system notif
+      300: "#2F2F2F", // color of duller modal
+      400: "#595959",
+      500: "#fff", // Search Bar lighter
+      600: "#dae4f7", // other chat bubble color
+      700: "#A5A5A5",
+      800: "#fff", // Header Color, Dark Gray
+      900: "#f4f2ef",
+    },
+  },
+};
+
+export const theme = extendTheme(config);
 
 export function Chakra({ cookies, children }: { cookies: string, children: ReactFragment }) {
   const colorModeManager =
@@ -13,52 +57,15 @@ export function Chakra({ cookies, children }: { cookies: string, children: React
       ? cookieStorageManager(cookies)
       : localStorageManager;
 
-  const theme = extendTheme({
-    initialColorMode: 'dark',
-    useSystemColorMode: false,
-    styles: {
-      global: (props: {
-        colorMode: 'dark',
-        theme: object,
-      }) => ({
-        'body': {
-          bg: mode('light.900', 'dark.900')(props),
-        },
-      }),
-    },
-    colors: {
-      dark: {
-        100: "#fff", // white
-        200: "#C9C9C9", // color of time meta and system notif
-        300: "#A5A5A5", // color of duller modal
-        400: "#595959",
-        500: "#37444D", // Search Bar lighter
-        600: "#37444d",
-        700: "#2F2F2F",
-        800: "#1F2125", // Header Color, Dark Gray
-        900: "#000002",
-      }, // invert the colors
-      light: {
-        100: "#0766c2", // white
-        200: "#1F2125", // color of time meta and system notif
-        300: "#2F2F2F", // color of duller modal
-        400: "#595959",
-        500: "#fff", // Search Bar lighter
-        600: "#dae4f7", // other chat bubble color
-        700: "#A5A5A5",
-        800: "#fff", // Header Color, Dark Gray
-        900: "#f4f2ef",
-      },
-    },
-  });
-
   return (
     <ChakraProvider
       resetCSS={false}
       theme={theme}
       colorModeManager={colorModeManager}
     >
-      {children}
+      <DarkMode>
+        {children}
+      </DarkMode>
     </ChakraProvider>
   );
 }
--- boards-app/pages/index.tsx
+++ boards-app/pages/index.tsx
@@ -2,11 +2,13 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useColorMode } from '@chakra-ui/react';
 
 const Home: NextPage = () => {
+  const { colorMode } = useColorMode();
   return (
-    <div>new project</div>
+    <div>{colorMode}</div>
   )
 }
 
-export default Home
+export default Home;
